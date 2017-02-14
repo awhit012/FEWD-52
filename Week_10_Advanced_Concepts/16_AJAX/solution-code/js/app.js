ecf7501597395b1f4298796fc17966dd997c9381
Alex White <awhit012@gmail.com>
2017-02-13 20:21:02 -0800

    adds solution

---
+++ Week_10_Advanced_Concepts/16_AJAX/solution-code/js/app.js
@@ -0,0 +1,37 @@
+var base_url = "http://api.giphy.com/v1/gifs/search?q=";
+var api_key = "&api_key=dc6zaTOxFJmzC";
+
+$('form').submit(function(event) {
+	event.preventDefault();
+	var search_string = $('input').val();
+	getGifs(search_string);
+});
+
+function getGifs(search_string) {
+	$.get(base_url + search_string + api_key, function(response){
+		var gif_url = response.data[0].images.original.url;
+		$('img').attr('src', gif_url);
+		putResults(response.data)
+	});
+}
+
+function putResults(resultsArray) {
+	resultsArray.forEach(function(element) {
+		console.log(element);
+	})
+}
+
+
+// var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+
+// $.get(url, function(response) {
+// 	console.log(response);
+// })
+
+// $.ajax({
+// 	method: 'GET',
+// 	url: url,
+// 	success: function(response) {
+// 		console.log(response);
+// 	}
+// });
